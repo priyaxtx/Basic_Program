5fce54ca3293a1239c2fd20ee6342cefb2c03c24
priyaxtx <priyatripathi44222@gmail.com>
2021-07-25 00:27:19 +0530

    completed

--- src/data_structure/Binary_search_tree_program.java
+++ src/data_structure/Binary_search_tree_program.java
@@ -4,19 +4,26 @@ public class Binary_search_tree_program{
 	
 	void BinarySearchTree(int a[],int first,int last,int key,int mid)
 	{
-		//int m=mid;
+		//int temp;
+	
 		while(first<=last){
 			if(a[mid]<key)
 			{
-				
+				first=mid+1;
+				mid=mid+1;
+				//temp=a[first];
 			}
-			if(key==mid)
+			else if(key==a[mid])
 			{
-			System.out.println("the value of the search number is this :"+key);	
+			System.out.println("the value of the search number index is :"+mid);	
+			break;
 			}
+			
 			else
 			{
-				
+				first=mid-1;
+				mid=mid-1;
+				//temp=a[first];
 			}
 			
 		}
@@ -27,7 +34,8 @@ public static void main(String[] args) {
 	int a[]= {12,13,14,15,16,17,18,19,20};
 	int last=a.length-1;
 int mid=a.length/2;
-int key=17;
+System.out.println(mid+" "+a[mid]);
+int key=12;
 Binary_search_tree_program s=new Binary_search_tree_program();
 s.BinarySearchTree(a,0,last,key,mid);
 	}
