60f51f64bfd05f0fadb81d9988e7e2e2b6e4fc99
priyaxtx <priyatripathi44222@gmail.com>
2021-07-28 23:55:47 +0530

    Made Node inner class

--- src/data_structure/Circular_Linked_list.java
+++ src/data_structure/Circular_Linked_list.java
@@ -1,7 +1,16 @@
 package data_structure;
 
 public class Circular_Linked_list {
+ class Node
+ {
+	int data;
+Node next;
 
+	Node(int data)
+	 {
+		 this.data=data;
+	 }
+ }
 	public static void main(String[] args) {
 		
 
