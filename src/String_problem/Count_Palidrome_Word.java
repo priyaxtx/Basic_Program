76735513fbddda07a92b74009b9cc3011c2bb5d8
priyaxtx <priyatripathi44222@gmail.com>
2021-08-09 23:56:37 +0530

    changes made in count of palidrome words

--- src/String_problem/Count_Palidrome_Word.java
+++ src/String_problem/Count_Palidrome_Word.java
@@ -1,10 +1,14 @@
 package String_problem;
 import java.util.Scanner;
 public class Count_Palidrome_Word {
+	int count=0;
+	String str=" ";
+	String word,word_temp=" ";
 	public  boolean check_Every_Single_Word_func(String word_temp)
 	{
 		
 		int word_temps_length=word_temp.length();
+		System.out.println(word_temp);
 		for(int i=0;i<=word_temps_length;i++,word_temps_length--)
 		{
 			if(word_temp.charAt(word_temps_length-1)!=(word_temp.charAt(i)))
@@ -17,15 +21,14 @@ public class Count_Palidrome_Word {
 	
 	public  void Taking_String_from_User()
 	{
-		int count=0;
-		String str=" ";
-		String word,word_temp=" ";
+		
 		int i=0;
 		Scanner sc=new Scanner (System.in);
 		System.out.println("Enter the sentence to check the number of words are palidrome: ");
 		word=sc.nextLine();
+		System.out.println(word);
 		int length_of_word=word.length();
-		for( i=length_of_word-1;i>=0;i++)
+		for(i=length_of_word-1;i>=1;i--)
 		{
 			 str=str+word.charAt(i);
 			
@@ -33,12 +36,14 @@ public class Count_Palidrome_Word {
 		if(str!=" ")
 		{
 			word_temp=word_temp+word.charAt(i);
-			
+			System.out.println(word_temp);
 		}
 		else 
+			System.out.println("else part" + word_temp);
 			if(check_Every_Single_Word_func(word_temp))
 			{
 			 	count++;
+			 	System.out.println(word);
 			 	word=" ";
 				}
 		}}
