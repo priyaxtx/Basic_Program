8469269abe09796711ebc2eeaa9cec3896169bce
priyaxtx <priyatripathi44222@gmail.com>
2021-09-02 20:09:27 +0530

    simple basic class

---
+++ src/guess_output/Booloperators.java
@@ -0,0 +1,10 @@
+package guess_output;
+
+public class Booloperators {
+	 public static void main(String args[]) 
+     {
+         boolean var1 = true;
+	    boolean var2 = false;
+	    System.out.println((var1 & var2));
+     } 
+}
